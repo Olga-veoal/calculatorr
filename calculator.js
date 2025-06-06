04429fccd0e7d9bb97581f6dbca3dd6fe780d175
Olga-veoal <oook12309@gmail.com>
2025-06-07 02:30:02 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,25 +1,5 @@
 <<<<<<< HEAD
-function add(a, b) {
-  return a + b;
-}
 
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-module.exports = { add, subtract, multiply };
-=======
-function divide(a, b) {
-  if(b === 0) throw new Error("Деление на ноль!");
-  return a / b;
-}
-
-module.exports = { add, subtract, multiply, divide };
->>>>>>> 14059b0 (Реализовано деление с проверкой на ноль)
 function add(a, b) {
   return a + b;
 }
